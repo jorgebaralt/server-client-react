6fd43e78037dc01ea778d89912ac9d29cd76c72a
jorgebaralt <jorgebaraltq@gmaik.com>
2018-10-24 14:28:22 -0400

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.end.NODE_ENV === 'production') {
+	//we are in production return prod keys
+	module.exports = require('./prod');
+} else {
+	// we are in dev return dev keys
+	module.exports = require('./dev');
+}
